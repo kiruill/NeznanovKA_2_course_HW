f0def0bf3dd9eef113ff5275efbbe1d2b241bf84
Незнанов Кирилл Андреевич <KNeznanov@sportmaster.ru>
2019-09-18 17:55:25 +0300

    Initial Commit

---
+++ NeznanovKA_HW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeznanovKA_HW1
+//
+//  Created by Neznanov Kirill Andreevich on 18/09/2019.
+//  Copyright © 2019 Neznanov Kirill Andreevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
