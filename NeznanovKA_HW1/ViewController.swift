80febd68c87bda98970f7e9a89450f7ea87e8598
Незнанов Кирилл Андреевич <KNeznanov@sportmaster.ru>
2019-09-19 14:18:23 +0300

    working square

--- NeznanovKA_HW1/ViewController.swift
+++ NeznanovKA_HW1/ViewController.swift
@@ -18,30 +18,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redColorView.backgroundColor = #colorLiteral(red: 0.5542624676, green: 0.05161597182, blue: 0.02569532127, alpha: 1)
+        yellowColorView.backgroundColor = #colorLiteral(red: 0.5506193329, green: 0.4922245629, blue: 0.07830767883, alpha: 1)
+        greenColorView.backgroundColor = #colorLiteral(red: 0.1704105357, green: 0.5628845531, blue: 0.0493475712, alpha: 1)
+        
 
     }
+
+    var count = 0
     
-    @IBAction private func changeColor(_ sender: UIButton) {
+    @IBAction private func changeColor() {
         //print(1)
         
-        enum lightColor {
-            case redLight
-            case yellowLight
-            case greenLight
-        }
+//        enum lightColor: Int{
+//            case redLight = 0
+//            case yellowLight = 1
+//            case greenLight = 2
+//        }
         
-//        redColorView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-//        yellowColorView.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
-//        greenColorView.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-        
-        
-        switch index {
-        case lightColor.redLight:
-            <#code#>
-        default:
-            <#code#>
+        if  count == 0 {
+            greenColorView.backgroundColor = #colorLiteral(red: 0.1704105357, green: 0.5628845531, blue: 0.0493475712, alpha: 1)
+            redColorView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            count += 1
+        } else if count == 1 {
+            redColorView.backgroundColor = #colorLiteral(red: 0.5542624676, green: 0.05161597182, blue: 0.02569532127, alpha: 1)
+            yellowColorView.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+            count += 1
+        } else if count == 2 {
+            yellowColorView.backgroundColor = #colorLiteral(red: 0.5506193329, green: 0.4922245629, blue: 0.07830767883, alpha: 1)
+            greenColorView.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+            count = 0
         }
     }
-    
 }
 
