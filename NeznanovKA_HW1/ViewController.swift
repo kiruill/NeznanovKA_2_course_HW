69057b3f1a5e6a4fe388472de75438266d71c9dc
Незнанов Кирилл Андреевич <KNeznanov@sportmaster.ru>
2019-09-19 13:19:59 +0300

    commit 3

--- NeznanovKA_HW1/ViewController.swift
+++ NeznanovKA_HW1/ViewController.swift
@@ -17,11 +17,30 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
     }
     
     @IBAction private func changeColor(_ sender: UIButton) {
-        print(1)
+        //print(1)
+        
+        enum lightColor {
+            case redLight
+            case yellowLight
+            case greenLight
+        }
+        
+//        redColorView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+//        yellowColorView.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+//        greenColorView.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        
+        
+        switch index {
+        case lightColor.redLight:
+            <#code#>
+        default:
+            <#code#>
+        }
     }
     
 }
